e21e110571a782b790f1d3441760d32b794ac575
Lewis Matthews <lewis.matthews@sainsburys.co.uk>
2022-01-06 13:41:03 +0000

    initial

---
+++ pub/script.js
@@ -0,0 +1,21 @@
+const refreshSeconds = 10;
+const envs = ['production', 'staging'];
+const container = document.getElementById('container');
+
+envs.map((env) => {
+    const div = document.createElement('div');
+    container.append(div);
+    const label = document.createElement('label');
+    label.innerText = 'Environment: ' + env;
+    div.append(label);
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('src', '/pub/envs/' + env);
+    iframe.setAttribute('id', env);
+    div.append(iframe);
+});
+
+setInterval(() => {
+    envs.map((env) => {
+        document.getElementById(env).contentWindow.location.reload();
+    });
+}, refreshSeconds * 1000)
